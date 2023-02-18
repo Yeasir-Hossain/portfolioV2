cde9c6af4d331fb85f064580f0f03c596cab5090
Yeasir-Hossain <yeasir06@gmail.com>
2023-02-18 17:32:51 +0600

    new

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,13 @@
 import '@/styles/globals.css'
+import Navbar from './Navbar'
+import { ThemeProvider } from 'next-themes'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <>
+    <ThemeProvider enableSystem={true} attribute='class'>
+      <Navbar></Navbar>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  </>
+
 }
