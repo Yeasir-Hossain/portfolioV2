3091d6352e437c7c96961bcef608f8ea95e199d1
Yeasir-Hossain <yeasir06@gmail.com>
2023-02-24 05:04:23 +0600

    done for now

--- pages/components/Experience/Experience.js
+++ pages/components/Experience/Experience.js
@@ -5,10 +5,10 @@ const Experience = () => {
     return (
         <div id='experience'>
             <SectionHeading heading={"Experience"} />
-            <h3 className='text-lg md:text-xl font-semibold'>Repsoft Consultancy Services Ltd. ,Hyderabad, India</h3>
-            <p className='text-base md:text-lg font-semibold'>Front-End Developer</p>
+            <h3 className='text-lg md:text-2xl font-semibold'>Repsoft Consultancy Services Ltd. ,Hyderabad, India</h3>
+            <p className='text-base md:text-xl font-semibold'>Front-End Developer</p>
             <p className='text-base md:text-lg font-semibold'>July, 2022 to October, 2022</p>
-            <div className='ml-2 mt-1 flex flex-col space-y-1 font-medium'>
+            <div className='ml-2 mt-1 flex flex-col space-y-1 font-medium text-lg'>
                 <p>1. Built fully responsive websites</p>
                 <p>2. Worked in a team building full stack projects</p>
                 <p>3. Single-handedly built a few websites</p>
--- pages/components/Skills/Skills.js
+++ pages/components/Skills/Skills.js
@@ -1,11 +1,49 @@
 import React from 'react';
 import SectionHeading from '../Shared/SectionHeading';
+import { AiFillHtml5 } from 'react-icons/ai'
+import { FaCss3, FaReact, FaNodeJs, FaCcStripe } from 'react-icons/fa'
+import { SiJavascript, SiTailwindcss, SiMongodb, SiFirebase } from 'react-icons/si'
+import { TbBrandNextjs } from 'react-icons/tb'
+import { BsFillBootstrapFill } from 'react-icons/bs'
 
 
 const Skills = () => {
     return (
         <div id='skills'>
             <SectionHeading heading={"Skills"} />
+            <div className='flex flex-wrap text-[85px] justify-evenly items-center my-6'>
+                <div>
+                    <AiFillHtml5 />
+                </div>
+                <div>
+                    <FaCss3 />
+                </div>
+                <div>
+                    <SiJavascript />
+                </div>
+                <div>
+                    <FaReact />
+                </div>
+                <div>
+                    <TbBrandNextjs />
+                </div>
+                <div>
+                    <SiTailwindcss />
+                </div>
+                <div>
+                    <BsFillBootstrapFill />
+                </div>
+                <div>
+                    <FaNodeJs />
+                </div>
+                <div>
+                    <SiMongodb />
+                </div>
+                <div>
+                    <SiFirebase />
+                </div>
+            </div>
+
 
         </div>
     );
