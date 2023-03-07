72995a38ba833f4570fb0ee7ec827bdd1fde2e24
Yeasir-Hossain <yeasir06@gmail.com>
2023-03-07 22:40:11 +0600

    resume updated

--- pages/components/Navigation/Navbar.js
+++ pages/components/Navigation/Navbar.js
@@ -65,7 +65,7 @@ const Navbar = () => {
                                 }
 
                             </button>
-                            <a href="https://drive.google.com/file/d/1gkWURnPqCspFmpbjdpcsWcCZSDhnpWag/view?usp=share_link" target="_blank" rel="noreferrer">
+                            <a href="https://drive.google.com/file/d/1oyQYOXXJL3GhT_ASKztPD_enlkHo4HAv/view?usp=share_link" target="_blank" rel="noreferrer">
                                 <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 hidden md:block  font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-purple-700 dark:hover:bg-purple-800">Resume</button>
                             </a>
                             {rendertheme()}
